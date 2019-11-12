bf04d2f3ddafe4ec457afde0558dfa77e7eeb201
Fengyuan Wu <wfy0425@yeah.net>
2019-11-11 21:24:03 -0800

    make css about logo banner global, copy content from student index into aboutus

--- pages/aboutus/aboutus.js
+++ pages/aboutus/aboutus.js
@@ -1,11 +1,70 @@
-// pages/aboutus/aboutus.js
+// pages/students/index/index.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    logoUrl: '/images/logo.svg',
+    bannerUrl: '/images/banner.svg',
+    imgUrls: [
+      'https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg',
+      'https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg',
+      'https://ss1.baidu.com/9vo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=92afee66fd36afc3110c39658318eb85/908fa0ec08fa513db777cf78376d55fbb3fbd9b3.jpg'
+    ],
+    isLoadedAll: false,
+    musicIndex: null,
+    videoIndex: null,
+    currentTabsIndex: 0,
+    pageIndex: 1,
+    photoList: [
+      {
+        'url': "/images/photo/多功能影音室.jpg",
+        'description': "这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。"
+      },
+      {
+        'url': "/images/photo/海洋泡泡区.jpg",
+        'description': "海洋泡泡区"
+      },
+      {
+        'url': "/images/photo/海洋球池.jpg",
+        'description': "海洋球池"
+      },
+    ],
+    videoList: [
+      {
+        'coverimg': "https://goss.veer.com/creative/vcg/veer/800water/veer-146156021.jpg",
+        'description': "这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。",
+        'id': "41",
+        'resource_add': "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+        'title': " 第三期 Beatles 02",
+        'type': "1"
+      },
+      {
+        'coverimg': "https://goss.veer.com/creative/vcg/veer/800water/veer-146156021.jpg",
+        'description': "",
+        'id': "42",
+        'resource_add': "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+        'title': " 第三期 Beatles 02",
+        'type': "1"
+      },
+      {
+        'coverimg': "https://goss.veer.com/creative/vcg/veer/800water/veer-146156021.jpg",
+        'description': "",
+        'id': "43",
+        'resource_add': "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+        'title': " 第三期 Beatles 02",
+        'type': "1"
+      },
+      {
+        'coverimg': "https://goss.veer.com/creative/vcg/veer/800water/veer-146156021.jpg",
+        'description': "",
+        'id': "44",
+        'resource_add': "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400",
+        'title': " 第三期 Beatles 02",
+        'type': "1"
+      },
+    ]
   },
 
   /**
@@ -62,5 +121,76 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+  //tap切换
+  onTabsItemTap: function (event) {
+    var index = event.currentTarget.dataset['index'];
+    this.setData({
+      currentTabsIndex: index
+    });
+    //tab切换时停止音乐播放
+    backgroundAudioManager.stop();
+
+    //tab切换时停止视频播放
+    var videoContextPrev = wx.createVideoContext('video' + this.data.videoIndex);
+    videoContextPrev.stop();
+
+    //将当前播放视频、音频的index设置为空
+    this.setData({
+      musicIndex: null,
+      videoIndex: null,
+    })
+  },
+  //展开
+  //原本没有upStatus这个字段，所以默认值为false
+  upDown(event) {
+    var index = event.currentTarget.dataset['index'];
+    this.data.videoList[index].upStatus = !this.data.videoList[index].upStatus;
+    this.setData({
+      videoList: this.data.videoList
+    })
+  },
+  //播放音频
+  musicPlay(event) {
+    var src = event.currentTarget.dataset['src'];
+    var index = event.currentTarget.dataset['index'];
+    this.setData({
+      musicIndex: index,
+      audioSrc: src
+    });
+
+    backgroundAudioManager.src = src;
+    backgroundAudioManager.play()
+
+  },
+  //停止音频
+  musicPause(event) {
+    this.setData({
+      musicIndex: null
+    });
+    backgroundAudioManager.pause();
+  },
+  //播放视频
+  videoPlay(event) {
+    var length = this.data.videoList.length;
+    var index = event.currentTarget.dataset['index'];
+
+    if (!this.data.videoIndex) { // 没有播放时播放视频
+      this.setData({
+        videoIndex: index
+      })
+      var videoContext = wx.createVideoContext('video' + index)
+      videoContext.play()
+    } else {
+      //停止正在播放的视频
+      var videoContextPrev = wx.createVideoContext('video' + this.data.videoIndex)
+      videoContextPrev.stop()
+      //将点击视频进行播放
+      this.setData({
+        videoIndex: index
+      })
+      var videoContextCurrent = wx.createVideoContext('video' + index)
+      videoContextCurrent.play()
+    }
+  },
 })
