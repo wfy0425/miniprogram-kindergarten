d900136971b6687b201b0fdb3b527b254009d18c
Fengyuan Wu <wfy0425@yeah.net>
2019-11-24 13:34:01 -0800

    implement the babyQA
    为育儿教育增加文章简介板块

--- pages/parents/babyQA/babyQA.js
+++ pages/parents/babyQA/babyQA.js
@@ -18,21 +18,24 @@ Page({
       'cloud://cloud-zhiai-8dv2t.636c-cloud-zhiai-8dv2t-1300754910/img/二层大教室角度A.jpg',
       'cloud://cloud-zhiai-8dv2t.636c-cloud-zhiai-8dv2t-1300754910/img/多功能影音室.jpg'
     ],
+    articleList: [
+      {
+        'url': "https://ss3.baidu.com/-fo3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=b5e4c905865494ee982209191df4e0e1/c2cec3fdfc03924590b2a9b58d94a4c27d1e2500.jpg",
+        'tittle': "这是文章题目",
+        'description': "这是文章简介"
+      },
+      {
+        'url': "https://ss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/image/h%3D300/sign=a62e824376d98d1069d40a31113eb807/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+        'tittle': "这是题目",
+        'description': "这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。这是第一个示例，这是第一个示例，这是第一个示例，这是第一个示例。"
+      },
+
+    ],  
     isLoadedAll: false,
     musicIndex: null,
     videoIndex: null,
     currentTabsIndex: 0,
     pageIndex: 1,
-    photoList: [
-      {
-        'url': "../../../images/photo/多功能影音室.jpg",
-        'description': "多功能影音室"
-      },
-      {
-        'url': "../../../images/photo/海洋球池.jpg",
-        'description': "海洋球池"
-      },
-    ],
   },
 
   /**
@@ -91,18 +94,5 @@ Page({
 
   },
 
-  onAccount: function (event) {
-    console.log(app.globalData.currentUserName);
-    if (app.globalData.currentUserName){
-      wx.navigateTo({
-        url: '/pages/parents/account/account',
-      })
-    }else{
-      //not login in
-      wx.navigateTo({
-        url: '/pages/login/login',
-      })
-    }
-  },
 
 })
